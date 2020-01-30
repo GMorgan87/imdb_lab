dbc6ef7bc52901bbcc1c3e560be5a666d657df7a
Gmorgan87 <gmorgan87@hotmail.co.uk>
2020-01-30 14:06:16 +0000

    Initial commit

---
+++ models/casting.rb
@@ -0,0 +1,9 @@
+class Casting
+
+  def initialize(options)
+    @id = options["id"].to_i if options["id"]
+    @movie-id = options["movie_id"]to_i
+    @star-id = options["star_id]"]to_i
+  end
+
+end
---
+++ models/star.rb
@@ -0,0 +1,10 @@
+class Star
+
+  def initialize(options)
+    @id = options["id"].to_i if options["id"]
+    @first_name = options["first_name"]
+    @last_name = options["last_name"]
+  end
+
+
+end
