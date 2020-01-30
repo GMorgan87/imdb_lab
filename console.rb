9aa28cf662057cfa1193b93dbe9771c36ec420a5
Gmorgan87 <gmorgan87@hotmail.co.uk>
2020-01-30 14:54:55 +0000

    adds save functions to models

---
+++ console.rb
@@ -0,0 +1,28 @@
+require('pry')
+require_relative('models/movie')
+require_relative('models/star')
+require_relative('models/casting')
+
+  movie1 = Movie.new({"title" => "Highlander", "genre" => "sci-fi" })
+  movie1.save()
+
+  movie2 = Movie.new({"title" => "Goldfinger", "genre" => "action" })
+  movie2.save()
+
+  star1 = Star.new({"first_name" => "Sean", "last_name" => "Connery"})
+  star1.save()
+
+  star2 = Star.new({"first_name" => "Christopher", "last_name" => "Lambert"})
+  star2.save()
+
+  casting1 = Casting.new({"movie_id" => movie1.id, "star_id" => star1.id})
+  casting1.save()
+
+  casting2 = Casting.new({"movie_id" => movie2.id, "star_id" => star1.id})
+  casting2.save()
+
+  casting3 = Casting.new({"movie_id" => movie1.id, "star_id" => star2.id})
+  casting3.save()
+
+binding.pry()
+nil
--- models/casting.rb
+++ models/casting.rb
@@ -1,9 +1,26 @@
+require_relative("../db/sql_runner")
+
 class Casting
+
+  attr_reader :id
+  attr_accessor :movie_id, :star_id
 
   def initialize(options)
     @id = options["id"].to_i if options["id"]
-    @movie-id = options["movie_id"]to_i
-    @star-id = options["star_id]"]to_i
+    @movie_id = options["movie_id"].to_i
+    @star_id = options["star_id"].to_i
   end
+
+  def save()
+    sql = "INSERT INTO castings
+    (movie_id, star_id)
+    values
+    ($1, $2)
+    RETURNING id"
+    values = [@movie_id, @star_id]
+    results = SqlRunner.run(sql, values)
+    @id = results[0]["id"].to_i
+  end
+
 
 end
